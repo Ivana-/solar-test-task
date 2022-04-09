7397275cb613a0abdb0413256d74d677b657be48
ivana- <ivana_2004@mail.ru>
2022-04-09 15:59:08 +0300

    typo

--- src/solar_test_task/search.clj
+++ src/solar_test_task/search.clj
@@ -51,18 +51,18 @@
 
 ;; we can define it ether as global channel - for limiting connections on any async income requests
 ;; or local one (see below in let form inside search - for limiting connections per each input request)
-;; (defonce ^:private con_ch (chan max-connections))
+;; (defonce ^:private con-ch (chan max-connections))
 
 (defn search [{:keys [params]}]
   (let [{:keys [tag]} params
         tags (set (if (string? tag) [tag] tag))
-        con_ch (chan max-connections) ;; may comment here & uncomment global one above
+        con-ch (chan max-connections) ;; may comment here & uncomment global one above
         res-ch (chan) ;; may make it buffered as (chan (count tags)) - it affects olny on little performance issues, not critical imho
         ]
     (doseq [tag tags]
-      (>!! con_ch 1)
+      (>!! con-ch 1)
       (go (let [res (request-tag-data tag)]
-            (<! con_ch)
+            (<! con-ch)
             (>! res-ch res))))
     (->> tags
          (reduce (fn [acc _] (merge-with (partial merge-with +) acc (<!! res-ch))) {})
